439c6450c076775070bfdcf541a4fa8ed0055a8a
WildDragonDot <chandan.mca.2019@gmail.com>
2023-10-24 16:14:59 +0530

    add

--- src/BMI_backend/main.mo
+++ src/BMI_backend/main.mo
@@ -1,5 +1,19 @@
 actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
-  };
+    public func calculateBMI(weightInKg: Float, heightInCM: Float) : async (Float, Text) {
+        let bmi = (weightInKg  / heightInCM / heightInCM)*10000;
+        let category =
+            if (bmi < 18.5) {
+                "Underweight"
+            } else if (bmi >= 18.5 and bmi < 24.9) {
+                "Normal weight"
+            } else if (bmi >= 24.9 and bmi < 29.9) {
+                "Overweight"
+            } else {
+                "Obesity"
+            };
+        
+        return (bmi, category);
+    }
+
 };
+
--- src/BMI_frontend/src/index.js
+++ src/BMI_frontend/src/index.js
@@ -1,19 +1,76 @@
-import { BMI_backend } from "../../declarations/BMI_backend";
+import {
+  BMI_backend
+} from "../../declarations/BMI_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
+
+var age = document.getElementById("age");
+var height = document.getElementById("height");
+var weight = document.getElementById("weight");
+var male = document.getElementById("m");
+var female = document.getElementById("f");
+var form = document.getElementById("form");
+let resultArea = document.querySelector(".comment");
+
+var modalContent = document.querySelector(".modal-content");
+var modalText = document.querySelector("#modalText");
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+
+
+document.getElementById("submit").addEventListener("click", async (e) => {
   e.preventDefault();
-  const button = e.target.querySelector("button");
+  const button = e.target;
 
-  const name = document.getElementById("name").value.toString();
+  // const name = document.getElementById("name").value.toString();
 
-  button.setAttribute("disabled", true);
+  button.disabled = true;
+  calculate()
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await BMI_backend.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
+  button.disabled = false;
 
   return false;
 });
+
+function calculate() {
+
+  if (age.value == '' || height.value == '' || weight.value == '' || (male.checked == false && female.checked == false)) {
+    modal.style.display = "block";
+    modalText.innerHTML = `All fields are required!`;
+  } else {
+    countBmi();
+  }
+
+}
+
+
+async function countBmi() {
+  var p = [age.value, height.value, weight.value];
+  if (male.checked) {
+    p.push("male");
+  } else if (female.checked) {
+    p.push("female");
+  }
+
+  const bmi_data = await BMI_backend.calculateBMI(parseFloat(p[2]), parseFloat(p[1]));
+
+  const bmi = bmi_data[0];
+  const result = bmi_data[1];
+
+  resultArea.style.display = "block";
+  document.querySelector(".comment").innerHTML = `You are <span id="comment">${result}</span>`;
+  document.querySelector("#result").innerHTML = bmi.toFixed(2);
+
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
